e9551dbf43339fc095d8a2a3f3a89ca5de61d448
Tomas Rodriguez <franco.tomas.rodriguez@gmail.com>
2026-04-06 08:15:49 -0300

    Ejercicio 5

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -4,4 +4,16 @@
 import os
 os.system("cls")
 
+marca = input("Ingrese la marca del coche: ")
+modelo = input("Ingrese el modelo del coche: ")
+año_fabr = int(input("Ingrese el año de fabricación del coche: "))
 class Coche:
+    def __init__ (self, marca, modelo, año_fabr):
+        self.marca = marca
+        self.modelo = modelo
+        self.año_fabr = año_fabr
+    def descripcion (self):
+        return f"Marca: {self.marca}, Modelo: {self.modelo}, Año de fabricación: {self.año_fabr}"
+
+coche = Coche(marca, modelo, año_fabr)
+print("Descripcion del Vehiculo: " + coche.descripcion())
